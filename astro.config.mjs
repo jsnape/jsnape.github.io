1f5e53e29f39acd2fa579a1e1928a1fc3eeba884
James Snape <james.snape@hitachivantara.com>
2024-01-12 17:48:39 +0000

    Major rewrite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jsnape.github.io',
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), svelte(), mdx()]
 });
