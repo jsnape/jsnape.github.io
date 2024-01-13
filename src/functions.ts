b71f90d1c8cbefe3eee82bcb70b50b32d25ffb64
James Snape <james.snape@hitachivantara.com>
2024-01-13 11:21:41 +0000

    Sticky header, single post component

---
+++ src/functions.ts
@@ -0,0 +1,27 @@
+import type {CollectionEntry} from "astro:content";
+
+/**
+ * Filter blog posts by published date and order them.
+ * Change the sort order by changing the minus sign to a plus sign, or add your new logic by changing the return value.
+ *
+ * You have access to the post data, so you can sort by any property you want, ex. by tags or title.
+ * The sort order is descending, so the newest posts are first.
+ *
+ * @param posts Collection of blog posts
+ * @returns Collection of blog posts sorted by date
+ */
+export const sortBlogPosts = (posts: CollectionEntry<'posts'>[]): CollectionEntry<'posts'>[] => {
+    return posts.sort((a, b) => {
+        return new Date(b.data.postDate).getTime() - new Date(a.data.postDate).getTime()
+    })
+}
+
+/**
+ * Exclude draft posts from the collection. If the site is built in production mode, draft posts are excluded by default.
+ *
+ * @param post Blog post
+ * @returns True if the post is not a draft
+ */
+export const excludeDrafts = ({data}: CollectionEntry<'posts'>): boolean => {
+    return import.meta.env.PROD ? !data.draft : true;
+}
