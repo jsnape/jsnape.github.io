aa6a37fb5138375051d874c96a771aa94b9ee882
James Snape <james.snape@hitachivantara.com>
2024-01-16 18:25:11 +0000

    Move base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jsnape.github.io',
-  base: '/',
   redirects: {
     '/posts': '/posts/1'
   },
