0dd72623810c17e21491900d14363bacf7aac0a6
James Snape <james.snape@hitachivantara.com>
2024-03-04 09:51:08 +0000

    Remove recent posts from homepage

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,6 @@ export const ITEMS_PER_PAGE = 5;
 /**
  * Number of recent posts to list.
  */
-export const RECENT_POSTS_COUNT = 7;
+export const RECENT_POSTS_COUNT = 10;
+
+export const HOME_PAGE_TITLE = 'Recent Posts';
