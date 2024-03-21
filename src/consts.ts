dd9f22ee73bafdbf7652ace514a33da6584b5681
James Snape <james.snape@hitachivantara.com>
2024-03-21 15:02:46 +0000

    Change homepage flow

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,6 @@ export const ITEMS_PER_PAGE = 5;
 /**
  * Number of recent posts to list.
  */
-export const RECENT_POSTS_COUNT = 10;
+export const RECENT_POSTS_COUNT = 11;
 
 export const HOME_PAGE_TITLE = 'Recent Posts';
